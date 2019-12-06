5bb62b2237dadc5f7304faff1c1112a6e3bb65d5
Guillaume Gautreau <guillaume@ghusse.com>
2019-12-07 00:57:39 +0100

    Day 6 part 2

--- src/06/index.ts
+++ src/06/index.ts
@@ -4,43 +4,91 @@ import { join } from "path";
 
 const readFileAsync = promisify(readFile);
 
+if (require.main === module) {
+	run()
+		.then((result) => console.info(result))
+		.catch((e) => {
+			console.error(e);
+			process.exitCode = 1;
+		});
+}
+
 export async function run(): Promise<string[]> {
 	const input = await readFileAsync(join(__dirname, "input.txt"), "utf8");
 
-	return [`${countOrbits(input)}`];
+	return [`${countOrbits(input)}`, `${findPathToSanta(input)}`];
 }
 
 export function countOrbits(input: string): number {
-	const nodesByKey: Map<string, INode> = input
-		.split("\n")
-		.reduce((nodes: Map<string, INode>, line: string): Map<string, INode> => {
-			const matches = /^([^)]+)\)([^)]+)/.exec(line);
+	const nodesByKey: Map<string, INode> = getNodesByKey(input);
 
-			if (!matches) {
-				throw new Error(`Unable to parse line ${line}`);
-			}
+	const allNodes = Array.from(nodesByKey.values());
 
-			const parentKey: string = matches[1]!;
-			const orbitingKey: string = matches[2]!;
+	return allNodes.map((node) => node.orbits).reduce((sum, orbits) => sum + orbits, 0);
+}
 
-			const parent: INode = nodes.get(parentKey) || { key: parentKey, orbits: 0, children: [] };
-			const orbiting: INode = nodes.get(orbitingKey) || {
-				key: orbitingKey,
-				orbits: 0,
-				children: [],
-				parent: parentKey,
-			};
+export function findPathToSanta(input: string): number {
+	const nodesByKey: Map<string, INode> = getNodesByKey(input);
 
-			orbiting.orbits++;
+	let you = nodesByKey.get("YOU")!;
+	let santa = nodesByKey.get("SAN")!;
 
-			parent.children.push(orbitingKey);
-			orbiting.parent = parentKey;
+	let moves: number = 0;
+	while (you.parent !== santa.parent) {
+		if (you.depth > santa.depth) {
+			you = nodesByKey.get(you.parent!)!;
+			moves++;
+		} else if (you.depth < santa.depth) {
+			santa = nodesByKey.get(santa.parent!)!;
+			moves++;
+		} else {
+			you = nodesByKey.get(you.parent!)!;
+			santa = nodesByKey.get(santa.parent!)!;
+			moves += 2;
+		}
+	}
 
-			nodes.set(parentKey, parent);
-			nodes.set(orbitingKey, orbiting);
+	return moves;
+}
 
-			return nodes;
-		}, new Map());
+function getNodesByKey(input: string): Map<string, INode> {
+	const nodesByKey = input.split("\n").reduce((nodes: Map<string, INode>, line: string): Map<
+		string,
+		INode
+	> => {
+		const matches = /^([^)]+)\)([^)]+)/.exec(line);
+
+		if (!matches) {
+			throw new Error(`Unable to parse line ${line}`);
+		}
+
+		const parentKey: string = matches[1]!;
+		const orbitingKey: string = matches[2]!;
+
+		const parent: INode = nodes.get(parentKey) || {
+			key: parentKey,
+			orbits: 0,
+			children: [],
+			depth: 0,
+		};
+		const orbiting: INode = nodes.get(orbitingKey) || {
+			key: orbitingKey,
+			orbits: 0,
+			children: [],
+			parent: parentKey,
+			depth: 0,
+		};
+
+		orbiting.orbits++;
+
+		parent.children.push(orbitingKey);
+		orbiting.parent = parentKey;
+
+		nodes.set(parentKey, parent);
+		nodes.set(orbitingKey, orbiting);
+
+		return nodes;
+	}, new Map());
 
 	const allNodes = Array.from(nodesByKey.values());
 
@@ -50,18 +98,19 @@ export function countOrbits(input: string): number {
 		throw new Error("Root not found");
 	}
 
-	propagateOrbitNumber(root, nodesByKey);
+	propagateOrbitNumberAndDepth(root, nodesByKey);
 
-	return allNodes.map((node) => node.orbits).reduce((sum, orbits) => sum + orbits, 0);
+	return nodesByKey;
 }
 
-function propagateOrbitNumber(root: INode, nodesByKey: Map<string, INode>) {
+function propagateOrbitNumberAndDepth(root: INode, nodesByKey: Map<string, INode>) {
 	for (let childKey of root.children) {
 		const child = nodesByKey.get(childKey)!;
 
 		child.orbits += root.orbits;
+		child.depth = root.depth + 1;
 
-		propagateOrbitNumber(child, nodesByKey);
+		propagateOrbitNumberAndDepth(child, nodesByKey);
 	}
 }
 
@@ -70,4 +119,5 @@ interface INode {
 	orbits: number;
 	children: string[];
 	parent?: string;
+	depth: number;
 }
--- src/06/orbits.spec.ts
+++ src/06/orbits.spec.ts
@@ -1,10 +1,11 @@
 import { expect } from "chai";
-import { countOrbits } from ".";
+import { countOrbits, findPathToSanta } from ".";
 
 describe("Day 6: Universal Orbit Map", orbitsSuite);
 
 function orbitsSuite() {
-	it("should give the right result for the example", countTest);
+	it("should give the right number of orbits for the example", countTest);
+	it("should give the right number of moves for the example", movesTest);
 
 	function countTest() {
 		const input: string = `COM)B
@@ -20,5 +21,23 @@ J)K
 K)L`;
 
 		expect(countOrbits(input)).to.eq(42);
+	}
+
+	function movesTest() {
+		const input: string = `COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`;
+
+		expect(findPathToSanta(input)).to.eq(4);
 	}
 }
