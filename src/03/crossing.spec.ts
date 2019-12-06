68987f05835c9f240e141bb7794788c673c2396a
Guillaume Gautreau <guillaume@ghusse.com>
2019-12-06 22:46:43 +0100

    Day 3 part 2

--- src/03/crossing.spec.ts
+++ src/03/crossing.spec.ts
@@ -1,33 +1,64 @@
 import { expect } from "chai";
-import { resolvePart1 } from ".";
+import { resolvePart1, resolvePart2 } from ".";
 
 describe("Day 3: Crossed Wires", day3Suite);
 
 function day3Suite() {
-	it(
-		"should return 6 with first input",
-		distanceTest.bind(undefined, `R8,U5,L5,D3\nU7,R6,D4,L4`, 6),
-	);
+	describe("Part 1", () => {
+		it(
+			"should return 6 with first input",
+			distanceTest.bind(undefined, `R8,U5,L5,D3\nU7,R6,D4,L4`, 6),
+		);
 
-	it(
-		"should return 159 with second input",
-		distanceTest.bind(
-			undefined,
-			`R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83`,
-			159,
-		),
-	);
+		it(
+			"should return 159 with second input",
+			distanceTest.bind(
+				undefined,
+				`R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83`,
+				159,
+			),
+		);
 
-	it(
-		"should return 135 with third input",
-		distanceTest.bind(
-			undefined,
-			`R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,
-			135,
-		),
-	);
+		it(
+			"should return 135 with third input",
+			distanceTest.bind(
+				undefined,
+				`R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,
+				135,
+			),
+		);
+	});
+
+	describe("Part 2", () => {
+		it(
+			"should return 30 with first input",
+			minStepsTest.bind(undefined, `R8,U5,L5,D3\nU7,R6,D4,L4`, 30),
+		);
+
+		it(
+			"should return 610 with second input",
+			minStepsTest.bind(
+				undefined,
+				`R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83`,
+				610,
+			),
+		);
+
+		it(
+			"should return 410 with third input",
+			minStepsTest.bind(
+				undefined,
+				`R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7`,
+				410,
+			),
+		);
+	});
 }
 
 function distanceTest(input: string, expectedResult: number) {
 	expect(resolvePart1(input)).to.eq(expectedResult);
+}
+
+function minStepsTest(input: string, expectedResult: number) {
+	expect(resolvePart2(input)).to.eq(expectedResult);
 }
--- src/03/index.ts
+++ src/03/index.ts
@@ -7,7 +7,7 @@ const readFileAsync = promisify(readFile);
 export async function run(): Promise<string[]> {
 	const input: string = await readFileAsync(join(__dirname, "input.txt"), "utf8");
 
-	return [`${resolvePart1(input)}`];
+	return [`${resolvePart1(input)}`, `${resolvePart2(input)}`];
 }
 
 interface IMovement {
@@ -16,7 +16,19 @@ interface IMovement {
 }
 
 export function resolvePart1(input: string): number {
-	const wireDefinitions: IMovement[][] = input.split("\n").map((line: string): IMovement[] =>
+	const wireDefinitions: IMovement[][] = parse(input);
+
+	return computeDistance(wireDefinitions);
+}
+
+export function resolvePart2(input: string): number {
+	const wireDefinitions: IMovement[][] = parse(input);
+
+	return computeMinSteps(wireDefinitions);
+}
+
+function parse(input: string): IMovement[][] {
+	return input.split("\n").map((line: string): IMovement[] =>
 		line.split(",").map(
 			(movement: string): IMovement => {
 				const parts = /([UDRL])([0-9]+)/.exec(movement);
@@ -40,12 +52,10 @@ export function resolvePart1(input: string): number {
 			},
 		),
 	);
-
-	return computeDistance(wireDefinitions);
 }
 
 export function computeDistance(wireDefinitions: IMovement[][]): number {
-	const wirePoints: Array<Set<string>> = wireDefinitions.map(computeAllPoints);
+	const wirePoints: Array<Map<string, IPoint>> = wireDefinitions.map(computeAllPoints);
 	const intersections = computeIntersections(wirePoints);
 
 	return intersections
@@ -53,15 +63,25 @@ export function computeDistance(wireDefinitions: IMovement[][]): number {
 		.reduce((min, distance) => Math.min(min, distance));
 }
 
-interface IPoint {
-	x: 0;
-	y: 0;
+function computeMinSteps(wireDefinitions: IMovement[][]): number {
+	const wirePoints: Array<Map<string, IPoint>> = wireDefinitions.map(computeAllPoints);
+	const intersections = computeIntersections(wirePoints);
+
+	return intersections
+		.map((point: IPoint) => point.steps)
+		.reduce((min, steps) => Math.min(min, steps));
 }
 
-function computeAllPoints(wireDefinition: IMovement[]): Set<string> {
-	const result: Set<string> = new Set();
+interface IPoint {
+	x: number;
+	y: number;
+	steps: number;
+}
 
-	let currentPoint = { x: 0, y: 0 };
+function computeAllPoints(wireDefinition: IMovement[]): Map<string, IPoint> {
+	const result: Map<string, IPoint> = new Map();
+
+	let currentPoint = { x: 0, y: 0, steps: 0 };
 
 	for (let movement of wireDefinition) {
 		const dx = movement.right > 0 ? 1 : movement.right < 0 ? -1 : 0;
@@ -76,28 +96,40 @@ function computeAllPoints(wireDefinition: IMovement[]): Set<string> {
 			currentPoint = {
 				x: currentPoint.x + dx,
 				y: currentPoint.y + dy,
+				steps: currentPoint.steps + 1,
 			};
 
-			result.add(JSON.stringify(currentPoint));
+			const key: string = JSON.stringify({ x: currentPoint.x, y: currentPoint.y });
+
+			if (!result.has(key)) {
+				result.set(key, currentPoint);
+			}
 		}
 	}
 
 	return result;
 }
 
-function computeIntersections(wirePoints: Array<Set<string>>): IPoint[] {
-	const intersections: Set<string> = new Set();
-	const allPoints: Set<string> = new Set();
+function computeIntersections(wirePoints: Array<Map<string, IPoint>>): IPoint[] {
+	const intersections: Map<string, IPoint> = new Map();
+	const allPoints: Map<string, IPoint> = new Map();
 
 	for (let wire of wirePoints) {
-		for (let point of wire.values()) {
+		for (let point of wire.keys()) {
 			if (allPoints.has(point)) {
-				intersections.add(point);
+				const first: IPoint = allPoints.get(point)!;
+				const second: IPoint = wire.get(point)!;
+
+				intersections.set(point, {
+					x: first.x,
+					y: first.y,
+					steps: first.steps + second.steps,
+				});
 			} else {
-				allPoints.add(point);
+				allPoints.set(point, wire.get(point)!);
 			}
 		}
 	}
 
-	return Array.from(intersections.values()).map((x) => JSON.parse(x));
+	return Array.from(intersections.values());
 }
