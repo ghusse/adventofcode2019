35cedc301a7d3aa54c74ae5e236443fa7644c637
Guillaume Gautreau <guillaume@ghusse.com>
2019-12-14 22:21:29 +0100

    Day 8 part 2

--- src/08/index.ts
+++ src/08/index.ts
@@ -7,7 +7,7 @@ const readFileAsync = promisify(readFile);
 export async function run(): Promise<string[]> {
 	const input: string = await readFileAsync(join(__dirname, "input.txt"), "utf8");
 
-	return [`${resolve1(input, 25, 6)}`];
+	return [`${resolve1(input, 25, 6)}`, `${resolve2(input, 25, 6)}`];
 }
 
 export function resolve1(input: string, width: number, height: number): number {
@@ -30,4 +30,24 @@ export function resolve1(input: string, width: number, height: number): number {
 	);
 
 	return minZeroLayer[1] * minZeroLayer[2];
+}
+
+export function resolve2(input: string, width: number, height: number): string {
+	const rawData = input.split("").map((x) => +x.trim());
+	const image: Array<number | undefined>[] = new Array(height)
+		.fill(undefined)
+		.map(() => new Array(width).fill(undefined));
+
+	for (let index = 0; index < input.length; index++) {
+		const layer = Math.floor(index / (width * height));
+		const digit = rawData[index];
+		const x = (index - layer * width * height) % width;
+		const y = Math.floor((index - layer * width * height) / width);
+
+		if (digit !== 2 && image[y][x] === undefined) {
+			image[y][x] = digit;
+		}
+	}
+
+	return image.map((line) => line.map((x) => (x === 1 ? "▨" : " ")).join("")).join("\n");
 }
--- src/08/spaceImage.spec.ts
+++ src/08/spaceImage.spec.ts
@@ -1,12 +1,19 @@
 import { expect } from "chai";
-import { resolve1 } from ".";
+import { resolve1, resolve2 } from ".";
 
 describe("Day 8: Space Image Format", spaceSuite);
 
 function spaceSuite(): void {
 	it("should return 1 in for the example", testResolve1.bind(null, "123456789012", 3, 2, 1));
+	it(
+		"should return 01/10 in for the example",
+		testResolve2.bind(null, "0222112222120000", 2, 2, " ▨\n▨ "),
+	);
 }
 
 function testResolve1(input: string, width: number, height: number, expectedResult: number) {
 	expect(resolve1(input, width, height)).to.eq(expectedResult);
+}
+function testResolve2(input: string, width: number, height: number, expectedResult: string) {
+	expect(resolve2(input, width, height)).to.eq(expectedResult);
 }
